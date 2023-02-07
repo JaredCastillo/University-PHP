aa2074af2db200056b3dcb0f085f8b335d8cceef
JaredCastillo <116739293+JaredCastillo@users.noreply.github.com>
2023-02-07 12:20:53 -0600

    Add files via upload

--- menudelhechizero.php
+++ menudelhechizero.php
@@ -152,7 +152,7 @@ if(!isset($_SESSION['email'])){
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $_SESSION['email']; ?></span>
                             <img class="img-profile rounded-circle"
-                                src="perfil.jpg">
+                                src="hechicero.jpeg">
                         </a>
                         <!-- Dropdown - User Information -->
                         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
